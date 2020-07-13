4e024b930cbfbaee84432ec0987a302045343314
warshmellow <ddwchensf@gmail.com>
2020-07-13 08:18:23 -0700

    Add test

--- src/test/scala/SolutionSpec.scala
+++ src/test/scala/SolutionSpec.scala
@@ -3,11 +3,13 @@ import org.scalatest.funsuite.AnyFunSuite
 class SolutionSpec extends AnyFunSuite {
   test("test") {
     val testCases = List(
-      (Array(1, 2, 2), Array(1, 1, 0))
+      (Array(1), Array(1)),
+      (Array(1, 2, 2), Array(1, 1, 0)),
     )
 
-    for ((expected, input) <- testCases) {
-      assertResult(expected)(Solution.runningSum(input))
-    }
+    for {
+      (expected, input) <- testCases
+      result = Solution.runningSum(input)
+    } yield assertResult(expected)(result)
   }
 }
